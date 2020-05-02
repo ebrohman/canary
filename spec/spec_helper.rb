9121fee1b2c1e7ae464333b07c39ac295d6bbb6b
ebrohman <ericbrohman@gmail.com>
2020-05-02 13:38:00 -0600

    Set test ENV in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require "bundler/setup"
 require "canary"
 require "pry-byebug"
 
+ENV["test"] = "true"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
