8d8e2a770113fec61b55da75a3f62ae4f2598c5b
ebrohman <ericbrohman@gmail.com>
2020-05-05 14:06:42 -0600

    Move CLI runner into lib/canary
    This is so that I can make an executable
    and call it easily, without having to deal
    with .thor files

--- lib/canary.rb
+++ lib/canary.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "canary/version"
+require "thor"
+require "json"
 
 module Canary
   class Error < StandardError; end
@@ -14,5 +16,71 @@ module Canary
 
   def self.root
     File.dirname __dir__
+  end
+
+  class Runner < Thor
+    desc "create", "create a new file"
+    method_option :file_path, required: true, aliases: "-f"
+    method_option :data, aliases: "-d"
+    method_option :file_mode, default: "w+"
+
+    def create
+      Canary::CreateFile
+        .new(file_path: options.file_path,
+             options: options.file_mode)
+        .call(options.data)
+    end
+
+    desc "modify", "modify an existing file"
+    method_option :file_path, required: true, aliases: "-f"
+    method_option :data, aliases: "-d"
+    method_option :file_mode, default: "a+"
+
+    def modify
+      Canary::ModifyFile
+        .new(file_path: options.file_path,
+             options: options.file_mode)
+        .call(options.data)
+    end
+
+    desc "delete", "delete an existing file"
+    method_option :file_path, required: true, aliases: "-f"
+
+    def delete
+      Canary::DeleteFile
+        .new(file_path: options.file_path)
+        .call
+    end
+
+    desc "request", "make a network request"
+    method_option :ssl, default: nil
+    method_option :type, default: "get"
+    method_option :headers, default: "{}"
+    method_option :body, default: nil
+    method_option :host, required: true
+    method_option :path, default: ""
+    method_option :port, default: "80"
+
+    allow_incompatible_default_type!
+
+    def request
+      Canary::MakeRequest
+        .new(ssl: options.ssl,
+             type: options.type,
+             headers: JSON.parse(options.headers),
+             body: options.body)
+        .call(options.host, options.path, options.port)
+    end
+
+    desc "spawn", "run an executable in a subprocess"
+    method_option :file_path, required: true, aliases: "-f"
+    method_option :options, default: nil
+
+    def spawn
+      Canary::Spawn
+        .new(file_path: options.file_path,
+             options: options.options)
+        .call
+    end
   end
 end
